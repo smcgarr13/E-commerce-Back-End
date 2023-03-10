bb8e5fe67abe344f407c73825a3bd1d2b1135dfb
Sparkles <smcgarr13@yahoo.com>
2023-03-10 12:34:14 -0600

    file-updates-models

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,54 @@
+// import necessary modules
 const { Model, DataTypes } = require('sequelize');
 
+// import database connection object
 const sequelize = require('../config/connection.js');
 
+const Product = require('./product');
+
+// define the category model as a subclass of sequelize's model class
 class Category extends Model {}
 
+// initialize the category model with the required fields
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    sequelize, // use the sequelize connection object
+    timestamps: false, // don't include timestamp fields
+    freezeTableName: true, // use the model name as the table name
+    underscored: true, // use snake_case for column names
+    modelName: 'category', // use the name 'category' for the model
   }
 );
 
+// Category.init(
+//   {
+//     // define columns
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'category',
+//   }
+// );
+
+// define a relationship between the Category and Product models
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // the foreign key for the relationship
+});
+
+// export the category model for use in other parts of the application
 module.exports = Category;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,63 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// initialize ProductTag model (table) by extending off sequelize's model class
 class ProductTag extends Model {}
 
+
+// set up fields and rules for ProductTag model
 ProductTag.init(
   {
-    // define columns
+    // define the id field as an integer primary key that auto-increments
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define the product_id field as an integer that references the product model's id
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    // define the tag_id field as an integer that references the tag model's id
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+    // define the metadata for the ProductTag model
+    sequelize,  // Use the sequelize connection object
+    timestamps: false,  // Don't include timestamp fields
+    freezeTableName: true,  // Use the model name as the table name
+    underscored: true,  // Use snake_case for column names
+    modelName: 'product_tag',  // Use the name 'product_tag' for the model
   }
 );
 
+
+// ProductTag.init(
+//   {
+//     // define columns
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'product_tag',
+//   }
+// );
+
+// export the ProductTag model for use in other parts of the application
 module.exports = ProductTag;
